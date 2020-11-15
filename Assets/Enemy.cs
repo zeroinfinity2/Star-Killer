70a0f0d12e2091e7b7be032dfeb06bdb93415528
zeroinfinity2 <73826185+zeroinfinity2@users.noreply.github.com>
2020-11-14 19:09:41 -0500

    Updates

---
+++ Assets/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _deathFX;
+    [SerializeField]
+    private Transform parent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        GameObject fx = Instantiate(_deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent.transform;
+        Destroy(this.gameObject);
+    }
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider shipCollider = gameObject.AddComponent<BoxCollider>();
+        shipCollider.isTrigger = false;
+    }
+}
---
+++ Assets/Scoreboard.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scoreboard : MonoBehaviour
+{
+    [SerializeField]
+    private int _scorePerHit;
+
+    private int _score = 0;
+
+    private Text _scoreText;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _scoreText = GetComponent<Text>();
+        _scoreText.text = _score.ToString();
+    }
+
+    public void ScoreHit()
+    {
+        _score += _scorePerHit;
+    }
+}
